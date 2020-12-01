624602ed6739f3c2b774fb6cdf7cc17533f816c3
Gustavo h. jobstraibizer <gustavo.henrique@uppercase.com.br>
2020-12-01 10:01:00 -0300

    chore: pesquisar produtos por nome

--- src/components/ListProducts/index.js
+++ src/components/ListProducts/index.js
@@ -9,6 +9,7 @@ export default function ListProducts() {
   let history = useHistory();
 
   const [products, setProducts] = useState([]);
+  const [term, setTerm] = useState("");
 
   useEffect(() => {
     const getProducts = async () => {
@@ -37,15 +38,52 @@ export default function ListProducts() {
     getProducts();
   }, []);
 
+  useEffect(() => {
+    const getProductsByName = async () => {
+      const getReference = async (path) => {
+        const res = await db.doc(path).get();
+        const data = res.data();
+        return data;
+      };
+
+      const db = app.firestore();
+
+      const res = await db.collection("products").where("name", "==", term).get();
+
+      const listItems = await Promise.all(
+        res.docs.map(async (doc) => {
+          let item = doc.data();
+          const result = await getReference(item.category.path);
+          item = { ...item, id: doc.id, category: result };
+          return item;
+        })
+      );
+
+      setProducts(listItems);
+    };
+
+    if (term) {
+      getProductsByName();
+    }
+  }, [term]);
+
   const handleEdit = (product) => {
     history.push(`produto/${product.id}`);
   };
 
   return (
     <>
-      <Link className="btn btn-success" to="/produto">
+      <Link className="btn btn-success mt-2" to="/produto">
         Novo
       </Link>
+
+      <input
+        type="text"
+        placeholder="Pesquisar nome produto"
+        onChange={(e) => setTerm(e.target.value)}
+        className="form-control mt-2"
+      />
+
       <Table bordered hover className="mt-2">
         <thead>
           <tr>
@@ -64,11 +102,7 @@ export default function ListProducts() {
               <td>{product.description}</td>
               <td>{product.category?.description}</td>
               <td>
-                <FontAwesomeIcon
-                  onClick={() => handleEdit(product)}
-                  className="text-info mr-2"
-                  icon={faEdit}
-                />
+                <FontAwesomeIcon onClick={() => handleEdit(product)} className="text-info mr-2" icon={faEdit} />
               </td>
             </tr>
           ))}
